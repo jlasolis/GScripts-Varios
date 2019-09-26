ea6606139982866c722daf566f24e35e502786a9
Jorge Luis Alvarez Solis <jorge.alvarez.solis@gmail.com>
2019-09-26 16:46:11 +0200

    Update 1-Todas-las-funciones.js

--- Funciones/1-Todas-las-funciones.js
+++ Funciones/1-Todas-las-funciones.js
@@ -1,3 +1,13 @@
+/*
+Funciones personalizadas útiles para SEM
+
+Acortar : devuelve URL acortada en TinyURL
+HTTPCodigo : Devuelve el Código HTTP (200, 404) de una URL
+UTMFacebook : devuelve la concatenacion de la URL dada con las UTMs para el etiquetado automático en Facebook
+
+*/
+
+
 function Acortar(url){
 url='http://tinyurl.com/api-create.php?url='+url;
 var response = UrlFetchApp.fetch(url,{muteHttpExceptions:true});
