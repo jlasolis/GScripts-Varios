f6b495b2a8792ae401793d5a73ba1c3bc0c2eceb
Jorge Luis Alvarez Solis <jorge.alvarez.solis@gmail.com>
2019-09-26 16:43:42 +0200

    Update 1-Todas-las-funciones.js

--- Funciones/1-Todas-las-funciones.js
+++ Funciones/1-Todas-las-funciones.js
@@ -9,3 +9,9 @@ var response = UrlFetchApp.fetch(url,{muteHttpExceptions:true});
 return response.getResponseCode()
 }
 
+function UTMFacebook(url){
+return url = url+"utm_medium=cpc&utm_source={{site_source_name}}&utm_campaign={{campaign.name}}_{{adset.name}}&utm_term={{placement}}&utm_content={{ad.name}}";
+  
+//esta es por idea de @josdiacam
+}
+
