0d623040a10f4b850980ca473784058ff827f420
Jorge Luis Alvarez Solis <jorge.alvarez.solis@gmail.com>
2019-10-09 23:11:29 +0200

    Corrección, falta inicio de parametros
    Por aviso de @albablanco , añadir el ? De inicio de parametros

--- Funciones/UTMFacebook.js
+++ Funciones/UTMFacebook.js
@@ -1,5 +1,5 @@
 function UTMFacebook(url){
-return url = url+"utm_medium=cpc&utm_source={{site_source_name}}&utm_campaign={{campaign.name}}_{{adset.name}}&utm_term={{placement}}&utm_content={{ad.name}}";
+return url = url+"?utm_medium=cpc&utm_source={{site_source_name}}&utm_campaign={{campaign.name}}_{{adset.name}}&utm_term={{placement}}&utm_content={{ad.name}}";
   
 //esta es por idea de @josdiacam
 }
